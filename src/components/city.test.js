bbb3078a92c47b6c2bbe1f5aa76ac828c9794dcb
unknown <eva.lapp24@gmail.com>
2019-04-30 15:06:10 -0600

    test cover

--- src/components/city.test.js
+++ src/components/city.test.js
@@ -8,8 +8,15 @@ test('test the class city', () => {
 	expect(city.howBig('Calgary')).toEqual('City');
 	expect(city.movedOut(1140000)).toEqual(100000);
 	expect(city.howBig('Calgary')).toEqual('Large Town');
+
 	expect(city.movedOut(99998)).toEqual(2);
 	expect(city.howBig('Calgary')).toEqual('Hamlet');
+	expect(city.movedOut(2)).toEqual(0);
+	expect(city.howBig('Calgary')).toEqual(null)
+	city.movedIn(120);
+	expect(city.howBig('Calgary')).toEqual("Village")
+	city.movedIn(1100);
+	expect(city.howBig('Calgary')).toEqual('Town')
 	});
 
 test ('test the class city default', () => {
@@ -18,11 +25,15 @@ test ('test the class city default', () => {
 });
 
 test('test the class community', () => {
-	const community = new Community();
+		const community = new Community();
+	expect(community.getMostNorthern()).toEqual('n/a')
+	expect(community.getMostSouthern()).toEqual('n/a')
 	expect(community.createCity()).toEqual([{city:'Whoville', latitude:-100, longitude:100, population:100, cityID:0}]);
 	community.createCity('Calgary', 51.0486, -114.0708, 12390000)
+	community.createCity('Equador', 0, 0, 0);
 	expect(community.whichSphere(1)).toEqual('Northern');
 	expect(community.whichSphere(0)).toEqual('Southern');
+	expect(community.whichSphere(2)).toEqual('Equator');
 	expect(community.getMostNorthern()).toEqual('Calgary');
 	expect(community.getMostSouthern()).toEqual('Whoville');
 	expect(community.getPopulationTotal()).toEqual(12390100)
@@ -30,7 +41,7 @@ test('test the class community', () => {
 	expect(community.showDetails(1)).toEqual({"city": "Calgary", "cityID": 1, "latitude": 51.0486, "longitude": -114.0708, "population": 12390000})
 	community.createCity('Edmonton', 56.8, -118.9, 400000);
 	community.deleteCity(1)
-	expect(community.showDetails(2)).toEqual({"city": "Edmonton", "cityID": 2, "latitude": 56.8, "longitude": -118.9, "population": 400000})
+	expect(community.showDetails(2)).toEqual( {"city": "Equador", "cityID": 2, "latitude": 0, "longitude": 0, "population": 0})
 	});
 
 test('test the object reference', () => {
