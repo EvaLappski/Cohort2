5d6097a844b1189463440709299c263325eecab9
unknown <eva.lapp24@gmail.com>
2019-05-01 11:41:24 -0600

    work on list class'

--- src/components/LinkedList.js
+++ src/components/LinkedList.js
@@ -3,28 +3,62 @@ import Node from './LinkedListNode'
 class LinkedList {
 	constructor (){
 			this.head = null;
-			// this.tail = null;
+			this.tail = null;
+			this.current = null;
 			this._length = 0;
 
 	}
 
-	add(a,b) {
-		let node = new Node(a,b)
-		// If there is no head, set node to head of list
-		if (!this.head){
-			this.head = node;
-			this._length += 1;
-			return node
-		}
-		let current = this.head;
-		while (current.next ){
-			current = current.next
-		}
+	insert(a,b) {
 
-		current.next = node;
-		this._length += 1;
-		return node
-	}	
+        // create the new node and place the data in it
+        const newNode = new Node(a,b);
+                
+        // special case: no nodes in the list yet
+        if (this.head === null) {
+            this.head = newNode;
+            this._length += 1;
+        } 
+        else {
+            // link the current tail and new tail
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this._length += 1;
+        }
+
+        // reassign the tail to be the new node
+        this.tail = newNode;
+    }
+
+    
+
+// 		let head = new Node(a,b);
+	
+// 		let secondNode = new Node(a,b);
+// 		head.next = secondNode
+// 		secondNode.previous = head;
+	
+// 		let thirdNode = new Node(a,b);
+// 		secondNode.next = thirdNode;
+// 		thirdNode.previous = secondNode;
+
+// 		let tail = thirdNode;
+
+// 		let current = head;
+
+// 		while (current !== null) {
+//     		console.log(current.data);
+//     		current = current.next;
+
+//     	let current = tail;
+
+// 		while (current !== null) {
+//   		  console.log(current.data);
+//    		 current = current.previous;
+// }
+
+// }
+	// }
 }
 
 export default LinkedList
--- src/components/LinkedList.test.js
+++ src/components/LinkedList.test.js
@@ -10,8 +10,17 @@ test('test the Node constructor', () => {
 
 test('test the LinkedList class', () => {
 	const testList = new LinkedList();
-	testList.add('Shane', 89)
-	console.log('the list', testList)
-	testList.add('Eva', 99)
-	console.log('the list again', testList)
+	// console.log('the list', testList)
+	testList.insert('Shane', 89)
+	// console.log('the list', testList)
+	testList.insert('Eva', 99)
+	// console.log('the list again', testList)
+	testList.insert('Dima', 79)
+	// console.log('the list once more', testList)
+	expect(testList.get(2)).toEqual('Shane')
+	console.log('get 0', testList.get(0));
+	console.log('get 1', testList.get(1));
+	console.log('get 2', testList.get(2))
+
+
 });
